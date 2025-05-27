cb38d27c7ce93e023a763f4d3d777a7fb93846e9
YagoHMz <160799923+YagoHMz@users.noreply.github.com>
2025-05-26 21:01:28 -0300

    dsadasdsa
    dasdasdas

--- PWIII/Controllers/DisciplinasController.cs
+++ PWIII/Controllers/DisciplinasController.cs
@@ -1,12 +1,91 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PWIII.Entidades;
 
 namespace PWIII.Controllers
 {
     public class DisciplinasController : Controller
     {
-        public IActionResult Index()
+        // GET: DisciplinasController
+        public ActionResult Index()
+        {
+            List<DisciplinaEntidade> model = new List<DisciplinaEntidade>();
+
+            DisciplinaEntidade item1 = new DisciplinaEntidade();
+            item1.Id = 1;
+            item1.Nome = "Programação Web";
+            item1.Ativo = true;
+            model.Add(item1);
+            return View(model);
+        }
+
+        // GET: DisciplinasController/Details/5
+        public ActionResult Details(int id)
         {
             return View();
+        }
+
+        // GET: DisciplinasController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DisciplinasController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DisciplinasController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DisciplinasController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DisciplinasController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DisciplinasController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
---
+++ PWIII/Entidades/DisciplinaEntidade.cs
@@ -0,0 +1,17 @@
+﻿namespace PWIII.Entidades
+{
+    public class DisciplinaEntidade
+    {
+        /*
+            CREATE TABLE DISCIPLINAS(
+                id INT PRIMARY KEY AUTO_INCREMENT,
+                Nome VARCHAR(150) NOT NULL,
+                ATIVO BIT DEFAULT 1
+
+         */
+
+        public int Id { get;set;}
+        public string Nome { get;set;}
+        public bool Ativo { get;set;}
+    }
+}
